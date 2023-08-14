f9b39889d6d783016b7e74a1e5357ec295f3d99e
RoshakN <msnooraniz@gmail.com>
2023-08-14 20:27:54 +0330

    feat: Add pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="text-3xl font-tsukimi bg-slate-400 text-white h-10 sticky flex justify-center items-center">
+      Navbar
+    </div>
+  );
+}
