10016b7a6d440a4fe4ce0b7165fee0cf21b1011b
RoshakN <msnooraniz@gmail.com>
2023-08-14 20:39:01 +0330

    feat: Add lorem to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,117 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia excepturi
+        ipsa quis cupiditate consequuntur! Explicabo veritatis ullam aspernatur
+        modi voluptas corporis, pariatur, alias rem ipsa cupiditate commodi
+        minus deserunt eligendi? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium tempora tenetur mollitia soluta, ex
+        obcaecati pariatur odio unde ea, odit cum error, quas enim nobis.
+        Asperiores provident atque magnam ab. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Deleniti nobis, incidunt facilis, quas nam
+        minus eius quam assumenda ad animi optio maxime, dignissimos praesentium
+        velit voluptate quasi officiis repudiandae delectus!{" "}
+      </p>
+    </div>
+  );
 }
