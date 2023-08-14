2ade5ec556c44dba2a388cf10a3433cd23f29d1f
RoshakN <msnooraniz@gmail.com>
2023-08-14 21:08:31 +0330

    feat: Add Links to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div className="text-3xl font-tsukimi bg-slate-400 text-white h-10 sticky flex justify-center items-center">
-      Navbar
+    <div className="text-base font-light font-dosis bg-slate-400 text-white sticky flex justify-between items-center px-5 py-2 sm:text-xl">
+      <h1 className="text-4xl font-tsukimi font-bold mr-7">The Blob</h1>
+      <div>
+        <Link to="/" className="mx-2">
+          HOME
+        </Link>
+        <Link to="/createpost" className="mx-2">
+          CREATE POST
+        </Link>
+        <Link to="/login" className="mx-2">
+          LOGIN
+        </Link>
+      </div>
+      <p className="ml-auto">icon</p>
     </div>
   );
 }
