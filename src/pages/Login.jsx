df48935c21d267a2228567c8db5d27570490ef9b
RoshakN <msnooraniz@gmail.com>
2023-08-14 21:23:07 +0330

    feat: Add Google button to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login page</div>;
+  return (
+    <div className="flex flex-col my-32 items-center font-dosis font-semibold">
+      <p className="w-52 text-center mb-10">
+        To make a Post in this blog, you must first log in using your Google
+        Account.
+      </p>
+      <button className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+        <img
+          className="w-6 h-6"
+          src="https://www.svgrepo.com/show/475656/google-color.svg"
+          loading="lazy"
+          alt="google logo"
+        />
+        <span>Login with Google</span>
+      </button>
+    </div>
+  );
 }
