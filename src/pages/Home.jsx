b12ad563b415a965a9d2be24e8220497e1f724dd
RoshakN <msnooraniz@gmail.com>
2023-08-23 12:27:53 +0330

    feat: Add exit button to header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
 import { dataBase } from "../services/firebase-config";
 import { Link } from "react-router-dom";
+import { CrossCircle } from "react-flaticons";
 
-export default function Home({isAuth}) {
+export default function Home({ isAuth }) {
   const [posts, setPosts] = useState([]);
+
+  const [showHeader, setShowHeader] = useState(true)
 
   const postsCollection = collection(dataBase, "posts");
 
@@ -12,12 +15,24 @@ export default function Home({isAuth}) {
     const getPosts = async () => {
       const data = await getDocs(postsCollection);
       setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data);
     };
     getPosts();
   }, []);
   return (
     <div>
-      <p className="my-4 text-xl text-center font-tsukimi">This is a public blog, so YOU can make a post too! Just head on over to <Link to={!isAuth ? "./login" : "./createpost"}><span className="cursor-pointer text-slate-500">this page</span></Link> and start telling your story :)</p>
+      {showHeader && <div className="relative">
+        <p className="py-4 text-xl text-center shadow-md font-tsukimi bg-gradient-to-br from from-slate-500 to to-slate-400 shadow-gray-400 text-neutral-50 md:text-2xl">
+          This is a public blog, so YOU can make a post too! Just head on over
+          to
+          <Link to={!isAuth ? "./login" : "./createpost"}>
+            <span className="cursor-pointer text-cyan-200">this page</span>
+          </Link>
+          and start telling your story :)
+        </p>
+        <CrossCircle size={14} onClick={() => {setShowHeader(!showHeader)}} className="absolute text-white hover:scale-125 hover:cursor-pointer right-2 top-2"/>
+      </div>}
+
       {posts.map((post) => {
         return (
           <div
